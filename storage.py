fb15aa8ee44fcb4232a8224183d0d3e6240a7625
simalgemicioglu <simal.gemicioglu@std.yeditepe.edu.tr>
2025-12-07 11:28:04 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,2 +1,7 @@
 import json
 import os
+from datetime import datetimefrom shutil import copyfile
+TASK_FILE = "data/tasks.json"
+CATEGORY_FILE = "data/categories.json"
+ACTIVITY_LOG = "data/activity.log"
+BACKUP_DIR = "backups/"
