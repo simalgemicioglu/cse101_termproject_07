33ba87d092402fffa4b362420b8437c34a859cfd
simalgemicioglu <simal.gemicioglu@std.yeditepe.edu.tr>
2025-12-07 12:03:30 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,6 +1,7 @@
 import json
 import os
-from datetime import datetimefrom shutil import copyfile
+from datetime import datetime
+from shutil import copyfile
 TASK_FILE = "data/tasks.json"
 CATEGORY_FILE = "data/categories.json"
 ACTIVITY_LOG = "data/activity.log"
@@ -10,10 +11,17 @@ def _load_json(filepath: str) -> list:
         return[]
     try:
         with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read()
             if not content:
                 return[]
             return json.loads(content)
     except (json.JSONecodeError, FileNotFoundError):
         print(f"Error: There was a problem loading the {filepath} file. Invalid format or file not found")
         return[]
-    
+def _save_json(filepath: str, data: list) -> None:
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    try:
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    except IOError as e:
+        print(f"Error: {filepath} There was a problem while saving: {e}")
